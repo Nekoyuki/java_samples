737416706cc374cd5281ce3f6b8a5d388ce278f3
Nekoyuki <Nekoyuki@hiyoko_x240>
2021-02-14 15:48:54 +0900

    edit

--- neko/src/constructor_sample/Main.java
+++
@@ -1,8 +0,0 @@
-package constructor_sample;
-
-public class Main {
-	public static void main(String[] args) {
-		PairsArray mmm = PairsArrayGen.createPairsArray("Hage", new Integer[]{0, 1, 2});
-		Integer i = 5;
-	}
-}
