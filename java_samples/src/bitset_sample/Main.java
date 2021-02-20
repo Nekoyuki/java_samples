8db6c53ea544da039203093c263fb6a3057db510
Nekoyuki <Nekoyuki@hiyoko_x240>
2021-02-20 20:23:54 +0900

    Bitset

---
+++ java_samples/src/bitset_sample/Main.java
@@ -0,0 +1,13 @@
+package bitset_sample;
+
+import java.util.BitSet;
+
+public class Main {
+	public static void main(String[] args) {
+		BitSet bitset = new BitSet();
+		bitset.set(1);
+		bitset.set(2);
+		bitset.set(4);
+		System.out.println(bitset);
+	}
+}
--- java_samples/src/constructor_sample/Main.java
+++ java_samples/src/constructor_sample/Main.java
@@ -1,16 +1,8 @@
 package constructor_sample;
 
-import java.util.BitSet;
-
-
 public class Main {
 	public static void main(String[] args) {
-	//	PairsArray mmm = PairsArrayGen.createPairsArray("Hage", new Integer[]{0, 1, 2});
-	//	Integer i = 5;
-	//}
-	BitSet aaa = new BitSet();
-	aaa.set(1);
-	aaa.set(2);
-
+		PairsArray mmm = PairsArrayGen.createPairsArray("Hage", new Integer[]{0, 1, 2});
+		Integer i = 5;
 	}
 }
